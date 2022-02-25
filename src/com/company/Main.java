89a565bf781780585bac0118aeece13762d85ff7
Sergey Rykov <sergrykov89@gmail.com>
2022-02-25 14:41:57 +0300

    правка

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,25 @@
 package com.company;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
         List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
-        Set<Integer> set = new HashSet();
+        List<Integer> list = new ArrayList<>();
 
 
         for (int i = 0; i < intList.size(); i++) {
             if (intList.get(i) > 0 && (intList.get(i) % 2) == 0 ) {
-                set.add(intList.get(i));
+                list.add(intList.get(i));
             }
         }
+        Collections.sort(list);
 
-        System.out.println(set);
-
-
-
+        System.out.println(list);
     }
+
 }
+
